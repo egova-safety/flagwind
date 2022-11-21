a845d7f49511b9ab6ca19440f3e8edc84c34a711
chendebao <chendebao@zhitongits.com.cn>
2022-11-21 20:23:00 +0800

    [M]增加repeat参数用来处理显示指定parent的层级

--- packages/web/src/annotations/receivable.ts
+++ packages/web/src/annotations/receivable.ts
@@ -6,18 +6,22 @@ import Vue from "vue";
  * @param uri 标注当前类型是一个广播接收器。
  * @param options 可选参数
  */
-export default function receivable(uri: string, options: { priority?: number; scope?: string } = { scope: "self", priority: 0 }) {
+export default function receivable(uri: string, options: { priority?: number; scope?: string; repeat?: number } = { scope: "self", priority: 0, repeat: 0 }) {
     if (!uri) {
         throw new flagwind.InvalidOperationException("The broadcast uri is empty.");
     }
 
     return function (target: any, name: any, descriptor: any) {
-
         let mounted = target.mounted;
         target.mounted = function () {
             let $this: Vue | any = this;
             if (options.scope === "parent") {
-                $this = $this.$parent;
+                $this = $this?.$parent;
+                let lv = Math.max(options.repeat || 0, 0);
+                while (lv > 0 && $this?.$parent) {
+                    $this = $this?.$parent;
+                    lv--;
+                }
             } else if (options.scope === "root") {
                 $this = $this.$root;
             }
